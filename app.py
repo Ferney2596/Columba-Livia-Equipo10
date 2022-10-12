f38598c76e81cfacfe1f7c09b1ce6ca3521efb4c
Ferney Moreno <ferney2596@gmail.com>
2022-10-11 20:56:55 -0500

    Actualizado para Heroku

--- app.py
+++ app.py
@@ -3,8 +3,6 @@ from app.models import User, Post, PostLikes
 
 app = create_app()
 
-if __name__ == "__main__":
-    app.run(debug=True, host="0.0.0.0", port=5000)
 
 @app.shell_context_processor
 def shell_context():
