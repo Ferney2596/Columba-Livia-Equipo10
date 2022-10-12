3fa32011feab20d1c09907dde3266ac106798d20
Ferney Moreno <ferney2596@gmail.com>
2022-10-11 20:34:12 -0500

    Actualizado app para Heroku

--- app.py
+++ app.py
@@ -3,7 +3,10 @@ from app.models import User, Post, PostLikes
 
 app = create_app()
 
+if __name__ == "__main__":
+    app.run(debug=True, host="0.0.0.0", port=5000)
 
 @app.shell_context_processor
 def shell_context():
     return {'db': db, 'User': User, 'Post': Post, 'PostLikes': PostLikes}
+
